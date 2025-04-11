c115ccb46f68544a0c8cb094c2f9451adf2d8a8c
electricmasquerade <electricmasquerade358@gmail.com>
2025-04-11 12:51:17 -0700

    Refactor particle count for testing and implement spatial partitioning in Simulation class

--- Simulation.cpp
+++ Simulation.cpp
@@ -5,20 +5,27 @@ float Simulation::smoothingFunction(const float r, const float h) { //r is the r
     // Implement the smoothing function here
     // using poly6 kernel for now
     if (r < h) {
-        float weight = 315.0f / (64.0f * M_PI * std::pow(h, 9)) * std::pow(h * h - r * r, 3);
+        const float weight = 315.0f / (64.0f * M_PI * std::pow(h, 9)) * std::pow(h * h - r * r, 3);
         return weight * pow((h*h - r*r), 3);
     }
     return 0.0f;
 }
 
-std::vector<Particle> Simulation::findNeighbors(const Particle &particle, const std::vector<Particle> &particles,
-    const float radius) {
-    std::vector<Particle> neighbors;
-    for (const auto &other : particles) {
-        if (&particle != &other && particle.getNeighborDistance(other) < radius) {
-            neighbors.push_back(other);
-        }
+void Simulation::buildSpatialMap(const std::vector<Particle> &particles) {
+    for (size_t i = 0; i < particles.size(); i++) {
+        Vec3 pos = particles[i].getPosition();
+        int cellX = static_cast<int>(pos[0] / cellSize);
+        int cellY = static_cast<int>(pos[1] / cellSize);
+        CellKey key{cellX, cellY};
+        spatialMap[key].push_back(particles[i]);
     }
+
+}
+
+std::vector<Particle> Simulation::findNeighbors(const Particle &particle, const std::vector<Particle> &particles,
+                                                const float radius) {
+    std::vector<Particle> neighbors;
+    //TODO: use spatial map to find neighbors
     return neighbors;
 }
 
--- Simulation.h
+++ Simulation.h
@@ -3,19 +3,41 @@
 #define SIMULATION_H
 #include <vector>
 #include "Particle.h"
+#include <cmath>
+#include <unordered_map>
 
+//Create hash functions and key structs for spatial partitioning
+struct CellKey {
+    int x, y;
+    bool operator==(const CellKey &other) const {
+        return x == other.x && y == other.y;
+    }
+};
+
+//create hash function for cell key
+struct CellKeyHash {
+    std::size_t operator()(const CellKey &key) const {
+        return std::hash<int>()(key.x) ^ std::hash<int>()(key.y);
+    }
+};
 
 class Simulation {
 
 public:
     float smoothingFunction(float r, float h);
+    void buildSpatialMap(const std::vector<Particle> &particles);
     std::vector<Particle> findNeighbors(const Particle &particle, const std::vector<Particle> &particles, float radius);
     void updateParticles(float dt);
 
 private:
     std::vector<Particle> particles;
+    float cellSize{20.0f}; // Size of each cell in the grid, replace with smoothing length later
+    std::unordered_map<CellKey, std::vector<Particle>, CellKeyHash> spatialMap; // Hash map for spatial partitioning
+    //Create a struct to store the keys for the particle grid during spatial partitioning
+
 
 };
+
 
 
 
